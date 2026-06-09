0275a95bb3e18ab5598e3ff13bc6a2c86a2a5446
Chandu Chikkudu <chanduchikkudu2004@gmail.com>
2026-06-09 19:10:23 +0530

    Day01-05

---
+++ Day05/Labs/CareBridge.PerformanceLab/CareBridge.PerformanceLab/Models/VwAnalyticsDeId.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CareBridge.PerformanceLab.Models;
+
+public partial class VwAnalyticsDeId
+{
+    public int EncounterId { get; set; }
+
+    public string AgeBand { get; set; } = null!;
+
+    public string Gender { get; set; } = null!;
+
+    public string EncounterType { get; set; } = null!;
+
+    public int DepartmentId { get; set; }
+}
